7c36c9cba4c48966c6541f5300ef39170409e7ff
VuMinhDuc20225705 <duc.vm225705@sis.hust.edu.vn>
2024-09-29 09:19:29 +0700

    Update Algebra_226.java

--- Algebra_226.java
+++ Algebra_226.java
@@ -1,73 +1,94 @@
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class Algebra_226 {
 	// Solving ax + b = 0
-	static void linear(int a, int b) {
+	static void linear(double a, double b) {
 		if (a == 0 && b == 0) {
-			System.out.println("There are infinite solutions.");
-		} else if (a == 0 && b != 0){
-			System.out.println("There are no solutions.");
+			JOptionPane.showMessageDialog(null, "There are infinite solutions.");
+		} else if (a == 0 && b != 0) {
+			JOptionPane.showMessageDialog(null, "There are no solutions.");
 		} else {
 			double x = -b / a;
-			System.out.println("Solution: x = " + x);
+			JOptionPane.showMessageDialog(null, "Solution: x = " + x);
 		}
 	}
 	
 	// Solving ax^2 + bx + c = 0
-	static void poly(int a, int b, int c) {
+	static void poly(double a, double b, double c) {
 		double d = b*b - 4*a*c;
 		double x, x1, x2;
 		
 		if (a == 0) {
 			linear(b, c);
 		} else if (d < 0){
-			System.out.println("There are no solutions.");
+			JOptionPane.showMessageDialog(null, "There are no solutions.");
 		} else if (d == 0) {
 			x = -b / (2*a);
-			System.out.println("Solution: x = " + x);
+			JOptionPane.showMessageDialog(null, "Solution: x12 = " + x);
 		} else {
 			x1 = (-b + Math.sqrt(d)) / (2*a);
 			x2 = (-b - Math.sqrt(d)) / (2*a);
-			System.out.println("Solutions: x1 = " + x1 + " and " + "x2 = " + x2);
+			JOptionPane.showMessageDialog(null, "Solutions: x1 = " + x1 + " and " + "x2 = " + x2 );
 		}
 	}
 	
 	// Solving 2-variable linear system
 	//     a11*x1 + a12*x2 = b1
 	//     a21*x1 + a22*x2 = b2
-	static void system(int a11, int a12, int b1, int a21, int a22, int b2) {
-		int d, dx, dy;
+	static void system(double a11, double a12, double b1, double a21, double a22, double b2) {
+		double d, dx, dy;
 		double x1, x2;
 		dx = b1 * a22 - a12 * b2;
 		dy = a11 * b2 - a21 * b1;
 		d = a11 * a22 - a12 * a21;
 		
 		if (d == 0 && (dx == 0 || dy == 0)) {
-			System.out.println("There are no solutions.");
+			JOptionPane.showMessageDialog(null, "There are no solutions.");
 		} else if (d == 0 && (dx != 0 && dy != 0)) {
-			System.out.println("There are infinite solutions.");
+			JOptionPane.showMessageDialog(null, "There are infinite solutions.");
 		} else {
 			x1 = dx / d;
 			x2 = dy / d;
-			System.out.println("Solutions: x1 = " + x1 + " and " + "x2 = " + x2);
+			JOptionPane.showMessageDialog(null, "Solutions: x1 = " + x1 + " and " + "x2 = " + x2 );
 		}
 	}
 	public static void main(String[] args) {
-		Scanner mode = new Scanner(System.in);
-		System.out.println("Choose your mode (1 = linear equation; 2 = 2x2 system; 3 = quadratic equation): ");
-		int m = mode.nextInt();
-		int[] arr = new int[6];
-		switch(m) {
-		case 1:
-			for (int i = 0; i < 2; i++) {
-				System.out.println("Enter your parameters in the order a to b (ax + b = 0)");
-				Scanner num = new Scanner(System.in);
-				int k = num.nextInt();
-				arr[i] = k;
-			}
-			linear(arr[0], arr[1]);
-		}
-	}
-}	
+		String initial; 
+        int choice; 
+        //JOptionPane.showMessageDialog(null, "Welcome to MyApplication"); 
+		  
+        initial = JOptionPane.showInputDialog(null, "1. Linear Equation\n2. Linear System\n3. Quadratic Equation"); 
+        choice = Integer.parseInt(initial); 
+        switch (choice) {
+        case 1:
+        	double a, b;
+        	a = Double.parseDouble(JOptionPane.showInputDialog(null, "Input a in 'ax + b = 0'"));
+        	b = Double.parseDouble(JOptionPane.showInputDialog(null, "Input b in 'ax + b = 0'"));
+        	linear(a, b);
+        	break;
+        case 2:
+        	double a11, a12, b1, a21, a22, b2;
+        	a11 = Double.parseDouble(JOptionPane.showInputDialog(null, "Input a11 in 'a11*x1 + a12*x2 = b1'"));
+        	a12 = Double.parseDouble(JOptionPane.showInputDialog(null, "Input a12 in 'a11*x1 + a12*x2 = b1'"));
+        	b1 = Double.parseDouble(JOptionPane.showInputDialog(null, "Input b1 in 'a11*x1 + a12*x2 = b1'"));
+        	a21 = Double.parseDouble(JOptionPane.showInputDialog(null, "Input a21 in 'a21*x1 + a22*x2 = b2'"));
+        	a22 = Double.parseDouble(JOptionPane.showInputDialog(null, "Input a22 in 'a21*x1 + a22*x2 = b2'"));
+        	b2 = Double.parseDouble(JOptionPane.showInputDialog(null, "Input b2 in 'a21*x1 + a22*x2 = b2'"));
+        	system(a11,a12,b1,a21,a22,b2);
+        	break;
+        case 3:
+        	double x, y, z;
+        	x = Double.parseDouble(JOptionPane.showInputDialog(null, "Input a in 'ax^2 + bx + c = 0'"));
+        	y = Double.parseDouble(JOptionPane.showInputDialog(null, "Input b in 'ax^2 + bx + c = 0'"));
+        	z = Double.parseDouble(JOptionPane.showInputDialog(null, "Input c in 'ax^2 + bx + c = 0'"));
+        	poly(x,y,z);
+        }
+        
+	}	
+}
+		
+		
+
+
 		
 
